1863ada424a4f8d8f4e40816a60b0e2efd8ca120
Benzekri <benzekrianass@gmail.com>
2022-11-08 14:10:55 +0100

    resoudre les conflits

--- src/main/java/fr/univtln/bab/project/resources/PersonneResource.java
+++ src/main/java/fr/univtln/bab/project/resources/PersonneResource.java
@@ -5,12 +5,14 @@ import fr.univtln.bab.project.daos.PersonneDAO;
 import fr.univtln.bab.project.entities.Personne;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Path("personnes")
 public class PersonneResource {
@@ -20,15 +22,64 @@ public class PersonneResource {
     EntityManager em = emf.createEntityManager();
 
 
+    /**
+     * this function returns all the personnes in the db thanks to a curl command
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Personne getAliens(){
+    public List<Personne> getPersons(){
         System.out.println("Personne called");
 
-        Personne A = em.find(Personne.class, 1);
-        System.out.println(A);
-        return A;
+        List<Personne> personnes = new ArrayList<>();
+
+        personnes=personneDAO.findAll();
+
+        return personnes;
 
     }
+
+    /**
+     * this function adds a person to the db thanks to a curl command
+     * @param a1 a personne written in json format
+     */
+    @POST
+    @Path("person")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void creatperson (Personne a1){
+
+        System.out.println("hunter created");
+        EntityTransaction transac = em.getTransaction();
+        transac.begin();
+        em.persist(a1);
+        transac.commit();
+    }
+
+
+    /**
+     * this function updates a person using his id thanks to a curl command
+     * @param id the old persons id
+     * @param t the new person
+     */
+    @PUT
+    @Path("person/update/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void updateperson(@PathParam("id") int id, Personne t) {
+        personneDAO.updatepersone(id,t);
+
+    }
+
+    /**
+     * this function deletes a person using his id thanks to a curl command
+     * @param id a persons id
+     */
+
+    @DELETE
+    @Path("person/delete/{id}")
+    public void removeperson(@PathParam("id") final int id){
+        personneDAO.remove(personneDAO.find(id));
+
+    }
+
+
 
 }
