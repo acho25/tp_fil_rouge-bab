bf875a1e26c3f868b8ba298df31175e95608b1d5
abderrazak bl <abderrazakbl@gmail.com>
2023-01-09 21:18:43 +0100

    but et equipe bean for jsf

---
+++ business/src/main/java/fr/univtln/bab/project/services/EquipeBean.java
@@ -0,0 +1,47 @@
+package fr.univtln.bab.project.services;
+
+import fr.univtln.bab.project.daos.EquipeDAO;
+import fr.univtln.bab.project.entities.Equipe;
+import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Stateless
+@Named
+public class EquipeBean {
+    @Inject
+    private EquipeDAO equipeDAO;
+
+    @Setter
+    @Getter
+    private Equipe equipe;
+
+    public EquipeBean() {
+        equipe = new Equipe();
+    }
+
+    public void init() {
+        equipe = new Equipe();
+    }
+    public Equipe getEq(){return equipe;}
+    public Equipe getEquipe(int id){
+        return equipeDAO.find(id);
+    }
+    public List<Equipe> getEquipes(){
+        return equipeDAO.findAll();
+    }
+    public void ajouterEquipe(Equipe equipe){
+        equipeDAO.persist(equipe);
+    }
+
+    public void modifierEquipe(Equipe equipeUpdated){
+        equipeDAO.update(equipeUpdated);
+    }
+
+    public void supprimerEquipe(Equipe equipe){
+        equipeDAO.remove(equipe);
+    }
+}
